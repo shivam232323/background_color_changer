f6ba22c39ddfbdae9434485d346422da00d431a6
shivam232323 <66739415+shivam232323@users.noreply.github.com>
2020-10-14 16:32:37 +0530

    first commit

---
+++ javascript.js
@@ -0,0 +1,18 @@
+var _ = require('lodash');
+console.log(_);
+
+var color1=document.getElementById("color1");
+var color2=document.getElementById("color2");
+var hold=document.getElementById("place");
+var button=document.getElementById("button");
+button.addEventListener("click",change_color);
+var body=document.getElementById("bodye");
+color1.addEventListener("input",change_color);
+color2.addEventListener("input",change_color);
+
+function change_color()
+{
+console.log(hold.value);
+body.style.background="linear-gradient(" +hold.value+ "," +color1.value +"," +color2.value+")";
+
+}
